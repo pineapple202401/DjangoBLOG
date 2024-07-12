41d62c9560b03faada21680b2b15e5a4e2ab4eae
pineapple202401 <pineapple202401@gmail.com>
2024-07-12 16:54:43 +0800

    20240712

--- DjangoBLOG/urls.py
+++ DjangoBLOG/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog_post.views import home
 
+#127.0.0.1:8000
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
--- blog_post/views.py
+++ blog_post/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from blog_post.models import Post
 # Create your views here.
+
+def home(requests):
+    # 1. 資料查詢 -> QuerySet Class
+    posts = Post.objects.all()
+
+    # 2. 處理資料
+    data_contain = list()
+    for count, post in enumerate(posts):
+        data_contain.append("<h2>#{}: {} </h2><hr>".format(str(count+1), post.title))
+        data_contain.append("<small>{}</small><br><hr>".format(post.content))
+    # 3. 渲染並回傳
+    return HttpResponse(data_contain)
